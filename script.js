e412c09ce784f50d18e4ad8397e94ed659c8a374
octavio-js <vizdoagaoctavio33@gmail.com>
2025-02-15 22:54:12 +0200

    Finish project

--- script.js
+++ script.js
@@ -1,7 +1,38 @@
 const mainDiv = document.querySelector(".main-div");
+const changeGridSizeButton = document.querySelector("#prompt-button");
 
-for(let i=0;i<256;i++){
-  let newDiv = document.createElement("div");
-  newDiv.classList.add("inside-div");
-  mainDiv.appendChild(newDiv);
+function createGrid(size){
+  mainDiv.innerHTML = "";
+  for(let i=0;i<size*size;i++){
+    const containerSize = 700;
+    const widthHeight = containerSize / size;
+
+    let div = document.createElement("div");
+    div.classList.add("inside-div");
+
+    div.style.width = `${widthHeight}px`;
+    div.style.height = `${widthHeight}px`;
+
+    div.addEventListener("mouseover", function () {
+      div.style.backgroundColor = "white";
+    });
+
+    mainDiv.appendChild(div);
+  }
 }
+
+function changeGridSize() {
+  let newSize;
+  while(true) {
+    newSize = parseInt(prompt("Enter new grid size(1-100): "));
+    if(newSize >= 1 && newSize <= 100){
+      break;
+    }
+    alert("Please enter a number between 1 and 100!");
+  }
+  createGrid(newSize);
+}
+
+createGrid(16);
+
+changeGridSizeButton.addEventListener("click", changeGridSize);
